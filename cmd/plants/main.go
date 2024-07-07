1c8c45ccb0cb1428ae4c7b2488353d52161cae6a
SevvyP <longisaac06@gmail.com>
2024-07-06 23:15:45 -0500

    basic GET endpoint with dummy data

--- cmd/plants/main.go
+++ cmd/plants/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/SevvyP/plants/internal/server"
+import (
+	"github.com/SevvyP/plants/internal/server"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := server.ResolveServer()
-	server.Start()
+	r := gin.Default()
+	r.StaticFile("/favicon.ico", "../public/favicon.ico")
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/plant/:name", server.HandleGetPlant)
+	}
+	r.Run()
 }
